1a53d280cb5eec2b08ad99c7c3e38d764ede3ef9
jsc5023 <kir5121@naver.com>
2023-02-08 16:01:56 +0900

    #1 - Querydsl 지원을 이용하여 api 검색 옵션 추가
    `QuerydslPredictateExecutor`,
    `QuerydslBinderCustomizer`를 이용하여 간단하게 개발

--- src/main/java/com/fastcampaus/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/fastcampaus/projectboard/repository/ArticleRepository.java
@@ -1,9 +1,29 @@
 package com.fastcampaus.projectboard.repository;
 
 import com.fastcampaus.projectboard.domain.Article;
+import com.fastcampaus.projectboard.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle> {
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
+        //bindings.bind(root.title).first(StringExpression::likeIgnoreCase); // like 's{v}' -> 퍼센트 필수로 직접 넣어줘야됨
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase); // like '%s{v}%' -> 퍼센트가 자동으로 들어감 exact match(정확하게 검색)이 아니라 like형식처럼 포함되면 검색할 수 있도록 변경하기 위해 사용
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
