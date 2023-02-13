ea83d7d57fd644006d662ff56da142b28179795d
jsc5023 <kir5121@naver.com>
2023-02-13 15:23:46 +0900

    #19 - diagrams.net에 #19브렌치 연동하기 위한 커밋

--- src/main/java/com/fastcampaus/projectboard/domain/Article.java
+++ src/main/java/com/fastcampaus/projectboard/domain/Article.java
@@ -25,7 +25,7 @@ import java.util.Set;
         @Index(columnList = "createdBy")
 })
 @Entity
-public class Article extends AuditingFields {
+public class Article extends AuditingFields  {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
