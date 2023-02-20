6d4fd84b7d6434dfabf18ca073bc55937f2b6748
jsc5023 <kir5121@naver.com>
2023-02-20 18:50:25 +0900

    #55 - dto 추가 리팩토링
    생성일, 생성자, 수정일, 수정자는 자동으로 만들어지므로, 해당 데이터의 값을 넣어주는 현상 수정

--- src/main/java/com/fastcampaus/projectboard/controller/ArticleController.java
+++ src/main/java/com/fastcampaus/projectboard/controller/ArticleController.java
@@ -84,7 +84,7 @@ public class ArticleController {
     public String postNewArticle(ArticleRequest articleRequest){
         // TODO: 인증 정보를 넣어줘야 한다.
         articleService.saveArticle(articleRequest.toDto(UserAccountDto.of(
-                "jsc", "asdf1234", "jsc@email.com", "Jsc", "memo", null, null, null, null
+                "jsc", "asdf1234", "jsc@email.com", "Jsc", "memo"
         )));
 
         return "redirect:/articles";
@@ -104,7 +104,7 @@ public class ArticleController {
     public String updateArticle(@PathVariable Long articleId, ArticleRequest articleRequest){
         // TODO: 인증 정보를 넣어줘야 한다.
         articleService.updateArticle(articleId, articleRequest.toDto(UserAccountDto.of(
-                "jsc", "asdf1234", "jsc@email.com", "Jsc", "memo", null, null, null, null
+                "jsc", "asdf1234", "jsc@email.com", "Jsc", "memo"
         )));
 
         return "redirect:/articles/" + articleId;
