1b9e31e4b139bcf18793572aa4a6400c4d8d2dd8
jsc5023 <kir5121@naver.com>
2023-03-06 18:00:23 +0900

    #94 - 인증 설정 업데이트: OAuth 인증 설정 추가
    스프링 시큐리티가 기존 DB 인증에다가 OAuth 인증 까지 받아들일 수 있도록 설정 추가
    해당 변경을 테스트 에도 반영

--- src/test/java/com/fastcampaus/projectboard/config/TestSecurityConfig.java
+++ src/test/java/com/fastcampaus/projectboard/config/TestSecurityConfig.java
@@ -1,7 +1,7 @@
 package com.fastcampaus.projectboard.config;
 
-import com.fastcampaus.projectboard.domain.UserAccount;
-import com.fastcampaus.projectboard.repository.UserAccountRepository;
+import com.fastcampaus.projectboard.dto.UserAccountDto;
+import com.fastcampaus.projectboard.service.UserAccountService;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.event.annotation.BeforeTestMethod;
@@ -13,17 +13,23 @@ import static org.mockito.BDDMockito.given;
 
 @Import(SecurityConfig.class)
 public class TestSecurityConfig {
-    @MockBean private UserAccountRepository userAccountRepository;
+    @MockBean private UserAccountService userAccountService;
 
     @BeforeTestMethod
     public void securitySetUp() {
-        given(userAccountRepository.findById(anyString())).willReturn(Optional.of(UserAccount.of(
+        given(userAccountService.searchUser(anyString()))
+                .willReturn(Optional.of(createUserAccountDto()));
+        given(userAccountService.saveUser(anyString(), anyString(), anyString(), anyString(), anyString()))
+                .willReturn(createUserAccountDto());
+    }
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(
                 "jscTest",
                 "pw",
                 "jsc-test@email.com",
                 "jsc-test",
                 "test memo"
-                )
-        ));
+        );
     }
 }
